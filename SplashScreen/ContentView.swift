f2bbfa0aabfce0f3681469bbc3c40f4d50859c38
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-01-08 02:20:03 +0700

    splash screen (on appear)

--- SplashScreen/ContentView.swift
+++ SplashScreen/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.blue.ignoresSafeArea()
+            Text("ContentView").foregroundColor(.white)
+                .font(.system(size: 30))
         }
-        .padding()
     }
 }
 
---
+++ SplashScreen/SplashScreenView.swift
@@ -0,0 +1,47 @@
+//
+//  SplashScreenView.swift
+//  SplashScreen
+//
+//  Created by jirakit on 8/1/2567 BE.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            VStack {
+                VStack {
+                    Image(systemName: "hare.fill")
+                        .font(.system(size: 80))
+                        .foregroundColor(.black)
+                    Text("Spalsh Screen")
+                        .font(Font.custom("Baskerville-Bold", size: 26))
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 1.2)){
+                        self.size = 0.9
+                        self.opacity = 1.0
+                    }
+                }
+            }
+            .onAppear{
+                DispatchQueue.main.asyncAfter(deadline: .now()+2.0) {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
