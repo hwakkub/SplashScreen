01e65ba3c4748f203ed4a3b35132ca5d15962aa7
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-01-08 02:02:59 +0700

    Initial Commit

---
+++ SplashScreen/SplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenApp.swift
+//  SplashScreen
+//
+//  Created by jirakit on 8/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
